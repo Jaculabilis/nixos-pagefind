3ee5ccc8b66184fa2a14da6c54453f34cd0c4355
Tim Van Baak <tim.vanbaak@gmail.com>
2025-05-19 08:38:51 -0700

    Flake init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
